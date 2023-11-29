c86e2392871dd2df59da5e95867329e2ea84f1c8
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2023-11-29 16:35:10 -0300

    ELints(correções)

--- src/index.js
+++ src/index.js
@@ -11,6 +11,25 @@ const HTTP_NOT_FOUND_STATUS = 404;
 const HTTP_UNAUTHORIZED_STATUS = 401;
 const HTTP_CREATED_STATUS = 201;
 const PORT = process.env.PORT || '3001';
+function isTokenValid(token) {
+  return token && token.length === 16;
+}
+
+function isDateValid(date) {
+  return /^\d{2}\/\d{2}\/\d{4}$/.test(date);
+}
+
+function isRateValid(rate) {
+  return typeof rate === 'number' && Number.isInteger(rate) && rate >= 1 && rate <= 5;
+}
+
+function validateTalkDetails({ watchedAt, rate }) {
+  if (!watchedAt) return 'O campo "watchedAt" é obrigatório';
+  if (!isDateValid(watchedAt)) return 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"';
+  if (rate === undefined) return 'O campo "rate" é obrigatório';
+  if (!isRateValid(rate)) return 'O campo "rate" deve ser um número inteiro entre 1 e 5';
+  return null;
+}
 
 async function readTalkerFile() {
   try {
@@ -34,10 +53,9 @@ app.get('/talker', async (_request, response) => {
   const talkers = await readTalkerFile();
   response.status(HTTP_OK_STATUS).json(talkers);
 });
-app.get('/talker/search', async (request, response) => {
+async function handleTalkerSearch(request, response) {
   const { q: searchTerm } = request.query;
   const { authorization: token } = request.headers;
-
 
   if (!token) {
     return response.status(HTTP_UNAUTHORIZED_STATUS).json({ message: 'Token não encontrado' });
@@ -52,11 +70,12 @@ app.get('/talker/search', async (request, response) => {
     return response.status(HTTP_OK_STATUS).json(talkers);
   }
 
-  
-  const filteredTalkers = talkers.filter(talker => talker.name.includes(searchTerm));
-
+  const filteredTalkers = talkers.filter((talker) => talker.name.includes(searchTerm));
   response.status(HTTP_OK_STATUS).json(filteredTalkers);
-});
+}
+
+app.get('/talker/search', handleTalkerSearch);
+
 app.get('/talker/:id', async (request, response) => {
   const { id } = request.params;
   const talkers = await readTalkerFile();
@@ -109,10 +128,6 @@ app.post('/login', (request, response) => {
   response.status(HTTP_OK_STATUS).json({ token });
 });
 
-function isTokenValid(token) {
-  return token && token.length === 16;
-}
-
 function validateName(name) {
   if (!name) return 'O campo "name" é obrigatório';
   if (name.length < 3) return 'O "name" deve ter pelo menos 3 caracteres';
@@ -126,19 +141,20 @@ function validateAge(age) {
   }
   return null;
 }
-
 function validateTalk(talk) {
   if (!talk) return 'O campo "talk" é obrigatório';
-  const { watchedAt, rate } = talk;
-  if (!watchedAt) return 'O campo "watchedAt" é obrigatório';
-  if (!/^\d{2}\/\d{2}\/\d{4}$/.test(watchedAt)) return 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"';
-  if (rate === undefined) return 'O campo "rate" é obrigatório';
-  if (typeof rate !== 'number' || !Number.isInteger(rate) || rate < 1 || rate > 5) {
-    return 'O campo "rate" deve ser um número inteiro entre 1 e 5';
-  }
-  return null;
+  return validateTalkDetails(talk);
 }
-
+function validateTalkerInput(name, age, talk) {
+  return validateName(name) || validateAge(age) || validateTalk(talk);
+}
+async function createNewTalker(name, age, talk) {
+  const talkers = await readTalkerFile();
+  const newTalker = { id: talkers.length + 1, name, age, talk };
+  talkers.push(newTalker);
+  await writeTalkerFile(talkers);
+  return newTalker;
+}
 app.post('/talker', async (request, response) => {
   const { authorization: token } = request.headers;
   if (!token) {
@@ -149,85 +165,59 @@ app.post('/talker', async (request, response) => {
   }
 
   const { name, age, talk } = request.body;
-  const nameError = validateName(name);
-  const ageError = validateAge(age);
-  const talkError = validateTalk(talk);
-
-  if (nameError || ageError || talkError) {
-    const message = nameError || ageError || talkError;
-    return response.status(HTTP_BAD_REQUEST_STATUS).json({ message });
+  const errorMessage = validateTalkerInput(name, age, talk);
+  if (errorMessage) {
+    return response.status(HTTP_BAD_REQUEST_STATUS).json({ message: errorMessage });
   }
 
-  const talkers = await readTalkerFile();
-  const newTalker = { id: talkers.length + 1, name, age, talk };
-  talkers.push(newTalker);
-  await writeTalkerFile(talkers);
-
+  const newTalker = await createNewTalker(name, age, talk);
   response.status(HTTP_CREATED_STATUS).json(newTalker);
 });
+function validateInput(name, age, talk) {
+  return validateName(name) || validateAge(age) || validateTalk(talk);
+}
 
 app.put('/talker/:id', async (request, response) => {
   const { id } = request.params;
   const { name, age, talk } = request.body;
   const { authorization: token } = request.headers;
-
-
   if (!token) {
     return response.status(HTTP_UNAUTHORIZED_STATUS).json({ message: 'Token não encontrado' });
-  }
-  if (!isTokenValid(token)) {
+  } if (!isTokenValid(token)) {
     return response.status(HTTP_UNAUTHORIZED_STATUS).json({ message: 'Token inválido' });
-  }
-
-  const nameError = validateName(name);
-  const ageError = validateAge(age);
-  const talkError = validateTalk(talk);
-
-  if (nameError || ageError || talkError) {
-    const message = nameError || ageError || talkError;
-    return response.status(HTTP_BAD_REQUEST_STATUS).json({ message });
-  }
-
-  const talkers = await readTalkerFile();
+  } const validationError = validateInput(name, age, talk);
+  if (validationError) {
+    return response.status(HTTP_BAD_REQUEST_STATUS).json({ message: validationError });
+  } const talkers = await readTalkerFile();
   const talkerIndex = talkers.findIndex((t) => t.id === parseInt(id, 10));
-  
   if (talkerIndex === -1) {
-    return response.status(HTTP_NOT_FOUND_STATUS).json({ message: 'Pessoa palestrante não encontrada' });
-  }
-
-  talkers[talkerIndex] = { ...talkers[talkerIndex], name, age, talk };
+    return response
+      .status(HTTP_NOT_FOUND_STATUS).json({ message: 'Pessoa palestrante não encontrada' });
+  } talkers[talkerIndex] = { ...talkers[talkerIndex], name, age, talk };
   await writeTalkerFile(talkers);
-
-
   response.status(HTTP_OK_STATUS).json(talkers[talkerIndex]);
 });
+
 app.delete('/talker/:id', async (request, response) => {
   const { id } = request.params;
   const { authorization: token } = request.headers;
-
-
   if (!token) {
     return response.status(HTTP_UNAUTHORIZED_STATUS).json({ message: 'Token não encontrado' });
-  }
-  if (!isTokenValid(token)) {
+  } if (!isTokenValid(token)) {
     return response.status(HTTP_UNAUTHORIZED_STATUS).json({ message: 'Token inválido' });
   }
-
   const talkers = await readTalkerFile();
-  const talkerIndex = talkers.findIndex((t) => t.id === parseInt(id, 10));
-  
+  const talkerIndex = talkers.findIndex((t) => t.id === parseInt(id, 10));  
   if (talkerIndex === -1) {
-    return response.status(HTTP_NOT_FOUND_STATUS).json({ message: 'Pessoa palestrante não encontrada' });
+    return response.status(HTTP_NOT_FOUND_STATUS).json({
+      message: 'Pessoa palestrante não encontrada' });
   }
 
   talkers.splice(talkerIndex, 1);
   await writeTalkerFile(talkers);
-
  
   response.status(204).send();
 });
-
-
 
 // Não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
