15d9cbe71c6bce73a8fabf0c0fc90ac4a2ccbd55
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2023-11-29 14:48:27 -0300

    re 5

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const fs = require('fs').promises;
-const crypto = require('crypto'); 
+const crypto = require('crypto');
 
 const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
 const HTTP_BAD_REQUEST_STATUS = 400;
+const HTTP_NOT_FOUND_STATUS = 404;
+const HTTP_UNAUTHORIZED_STATUS = 401;
+const HTTP_CREATED_STATUS = 201;
 const PORT = process.env.PORT || '3001';
 
 async function readTalkerFile() {
@@ -16,6 +19,14 @@ async function readTalkerFile() {
   } catch (error) {
     console.error('Erro ao ler o arquivo:', error.message);
     return [];
+  }
+}
+
+async function writeTalkerFile(data) {
+  try {
+    await fs.writeFile('src/talker.json', JSON.stringify(data));
+  } catch (error) {
+    console.error('Erro ao escrever no arquivo:', error.message);
   }
 }
 
@@ -31,7 +42,8 @@ app.get('/talker/:id', async (request, response) => {
   const talker = talkers.find((t) => t.id === parseInt(id, 10));
 
   if (!talker) {
-    return response.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    return response
+      .status(HTTP_NOT_FOUND_STATUS).json({ message: 'Pessoa palestrante não encontrada' });
   }
 
   response.status(HTTP_OK_STATUS).json(talker);
@@ -73,6 +85,64 @@ app.post('/login', (request, response) => {
 
   const token = crypto.randomBytes(8).toString('hex');
   response.status(HTTP_OK_STATUS).json({ token });
+});
+
+function isTokenValid(token) {
+  return token && token.length === 16;
+}
+
+function validateName(name) {
+  if (!name) return 'O campo "name" é obrigatório';
+  if (name.length < 3) return 'O "name" deve ter pelo menos 3 caracteres';
+  return null;
+}
+
+function validateAge(age) {
+  if (age === undefined) return 'O campo "age" é obrigatório';
+  if (typeof age !== 'number' || !Number.isInteger(age) || age < 18) {
+    return 'O campo "age" deve ser um número inteiro igual ou maior que 18';
+  }
+  return null;
+}
+
+function validateTalk(talk) {
+  if (!talk) return 'O campo "talk" é obrigatório';
+  const { watchedAt, rate } = talk;
+  if (!watchedAt) return 'O campo "watchedAt" é obrigatório';
+  if (!/^\d{2}\/\d{2}\/\d{4}$/.test(watchedAt)) return 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"';
+  if (rate === undefined) return 'O campo "rate" é obrigatório';
+  if (typeof rate !== 'number' || !Number.isInteger(rate) || rate < 1 || rate > 5) {
+    return 'O campo "rate" deve ser um número inteiro entre 1 e 5';
+  }
+  return null;
+}
+
+
+app.post('/talker', async (request, response) => {
+  const { authorization: token } = request.headers;
+  if (!token) {
+    return response.status(HTTP_UNAUTHORIZED_STATUS).json({ message: 'Token não encontrado' });
+  }
+  if (!isTokenValid(token)) {
+    return response.status(HTTP_UNAUTHORIZED_STATUS).json({ message: 'Token inválido' });
+  }
+
+  const { name, age, talk } = request.body;
+  const nameError = validateName(name);
+  const ageError = validateAge(age);
+  const talkError = validateTalk(talk);
+
+  if (nameError || ageError || talkError) {
+    const message = nameError || ageError || talkError;
+    return response.status(HTTP_BAD_REQUEST_STATUS).json({ message });
+  }
+
+  const talkers = await readTalkerFile();
+  const newTalker = { id: talkers.length + 1, name, age, talk };
+  talkers.push(newTalker);
+  await writeTalkerFile(talkers);
+
+  response.status(HTTP_CREATED_STATUS).json(newTalker);
 });
 
 // Não remova esse endpoint, é para o avaliador funcionar
